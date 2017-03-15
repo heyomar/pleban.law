d5ffb8e4eb18bd2eba99f340a1bf75314739bd33
Omar Rasool <omar@leveragenam.com>
2017-03-15 11:33:55 -0500

    ADDS - posts query for index page then adds single.php

--- index.php
+++ index.php
@@ -1,10 +1,24 @@
 <?php get_header(); ?>
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-		<p>
-			You've Landed on Index.php, go
-			<a href="/">Home!</a>
-		</p>
+		<?php
+		$args = array(
+			'posts_per_page' => -1,
+			'orderby'        => 'date',
+			'order'          => 'DESC',
+			'post_type'      => 'post',
+			'post_status'    => 'publish'
+
+		);
+		$lastposts = get_posts( $args );
+		foreach ( $lastposts as $post ) :
+		  setup_postdata( $post ); ?>
+			<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+			<?php the_excerpt() ?>
+		<?php endforeach;
+		wp_reset_postdata(); ?>
+
+<?php wp_reset_postdata(); ?>
   <?php endwhile; ?>
 	<?php endif ?>
 <?php get_footer(); ?>

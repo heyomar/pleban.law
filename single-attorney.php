17ce1faaf26905b96ff490aa3e6fbed2898eba92
Omar Rasool <omar@leveragenam.com>
2017-03-30 15:42:01 -0500

    ADDS - builds out more of the single attorney template 99%

--- single-attorney.php
+++ single-attorney.php
@@ -23,25 +23,98 @@
 
 
 						<div class="row">
+
 							<div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
 								<img class="profile-image" src="<?php the_field('profile_image'); ?>" alt="">
 								<a class="message-btn" href="">Send Message</a>
 							</div>
+
 							<div class="col-xs-12 col-sm-9 col-md-9 col-lg-9">
-								<h3><?php the_title(); ?></h3>
-								<h5><?php the_field("position"); ?></h5>
+								<div class="right-column">
+									<h3><?php the_title(); ?></h3>
+									<h5><?php the_field("position"); ?></h5>
 
-								<?php if( have_rows('quick_bio') ):
-									    while ( have_rows('quick_bio') ) : the_row(); ?>
+									<div class="quick-bio bottom-space">
+										<ul class="list">
+											<?php if( have_rows('quick_bio') ):
+												    while ( have_rows('quick_bio') ) : the_row(); ?>
+
+																	<li class="list-item"><?php the_sub_field('fact'); ?></li>
+
+													<?php endwhile; ?>
+											<?php else : endif; ?>
+										</ul>
+									</div>
 
 
-													<ul>
-														<li><?php the_sub_field('fact'); ?></li>
-													</ul>
+									<div class="bar-admissions bottom-space">
+										<h5 class="title">Bar Admissions</h5>
+											<?php if( have_rows('bar_dates') ):
+												    while ( have_rows('bar_dates') ) : the_row(); ?>
 
-									    <?php endwhile; ?>
+														<div class="row">
+															<div class="col-xs-2">
+																<div class="content">
+																	<strong><?php the_sub_field('year'); ?></strong>
+																	<br>
+																</div>
+															</div>
 
-									<?php else : endif; ?>
+															<div class="col-xs">
+																<div class="content">
+																	<?php the_sub_field('info'); ?>
+																	<br>
+																</div>
+															</div>
+														</div>
+													<?php endwhile; ?>
+											<?php else : endif; ?>
+									</div>
+
+
+									<div class="pro-memberships bottom-space">
+										<h5 class="title">Proffesional Memberships &amp; Associations</h5>
+											<div class="row">
+												<div class="col-xs-12">
+													<div class="content">
+														<?php if( have_rows('organizations') ):
+															while ( have_rows('organizations') ) : the_row(); ?>
+																<?php the_sub_field('organization'); ?><br>
+														<?php endwhile; ?>
+														<?php else : endif; ?>
+													</div>
+												</div>
+											</div>
+									</div>
+
+
+									<div class="other-experience bottom-space">
+										<h5 class="title">Other Experience &amp; Awards</h5>
+										<?php if( have_rows('awards') ):
+											    while ( have_rows('awards') ) : the_row(); ?>
+
+													<div class="row">
+														<div class="col-xs-3">
+															<div class="content">
+																<strong><?php the_sub_field('years'); ?></strong>
+																<br>
+															</div>
+														</div>
+
+														<div class="col-xs">
+															<div class="content">
+																<?php the_sub_field('description'); ?>
+																<br>
+															</div>
+														</div>
+													</div>
+
+												<?php endwhile; ?>
+										<?php else : endif; ?>
+									</div>
+
+
+								</div>
 							</div>
 						</div>
 

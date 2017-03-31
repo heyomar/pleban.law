2e5e9e55a49e29f97f965ef511fd6a2564fbcd16
Omar Rasool <omar@leveragenam.com>
2017-03-31 15:28:35 -0500

    FIXES - auto scroll on wrong part of js for menu

--- build/bundle.js
+++ build/bundle.js
@@ -9915,13 +9915,13 @@ Object.defineProperty(exports, "__esModule", {
 function navigation() {
 	$('.bars, .cross').on('click', function () {
 		if ($('.menu-open')[0]) {
-			$('body, html').css({ 'overflow-y': 'scroll' });
+			$('body, html').css({ 'overflow-y': 'auto' });
 			$('.menu-header-navigation-container').removeClass('menu-open');
 			$('.menu-header-navigation-container').animate({ right: '-350px' });
 			$('.cross').animate({ right: '-80px' }, 'fast');
 			$('.bars').animate({ right: '0px' }, 'fast');
 		} else {
-			$('body, html').css({ 'overflow-y': 'auto' });
+			$('body, html').css({ 'overflow-y': 'hidden' });
 			$('.menu-header-navigation-container').animate({ right: '0px' });
 			$('.cross').animate({ right: '0px' });
 			$('.bars').animate({ right: '-80px' }, 'fast');
--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,13 +1,13 @@
 function navigation() {
 	$('.bars, .cross').on('click', function() {
 		if ($('.menu-open')[0]) {
-				$('body, html').css({'overflow-y': 'scroll'});
+				$('body, html').css({'overflow-y': 'auto'});
 				$('.menu-header-navigation-container').removeClass('menu-open');
 				$('.menu-header-navigation-container').animate({right:'-350px'});
 				$('.cross').animate({right:'-80px'}, 'fast');
 				$('.bars').animate({right:'0px'}, 'fast');
 		} else {
-				$('body, html').css({'overflow-y': 'auto'});
+				$('body, html').css({'overflow-y': 'hidden'});
 				$('.menu-header-navigation-container').animate({right:'0px'});
 				$('.cross').animate({right:'0px'});
 				$('.bars').animate({right:'-80px'}, 'fast');

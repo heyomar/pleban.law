e5df810ee1e9ed5b3068d6b033bd94bb307561fd
Omar Rasool <omar@leveragenam.com>
2017-04-06 11:49:47 -0500

    FIXES - removes scroll locking when menu is open, adds elastic scroll for devices

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,13 +1,11 @@
 function navigation() {
 	$('.bars, .cross').on('click', function() {
 		if ($('.menu-open')[0]) {
-				$('body, html').css({'overflow-y': 'auto'});
 				$('.menu-header-navigation-container').removeClass('menu-open');
 				$('.menu-header-navigation-container').animate({right:'-350px'});
 				$('.cross').animate({right:'-80px'}, 'fast');
 				$('.bars').animate({right:'0px'}, 'fast');
 		} else {
-				$('body, html').css({'overflow-y': 'hidden'});
 				$('.menu-header-navigation-container').animate({right:'0px'});
 				$('.cross').animate({right:'0px'});
 				$('.bars').animate({right:'-80px'}, 'fast');
@@ -22,6 +20,7 @@ function navigation() {
 			$('.menu-header-navigation-container').css('right', '-350px');
 		}
 	});
+
 }
 
 export default navigation

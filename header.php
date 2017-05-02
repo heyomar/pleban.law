3f1b4a330351e3f433b3b1572478efd5f3bd65cb
Omar Rasool <omar@leveragenam.com>
2017-05-02 11:02:24 -0500

    ADDS - new typeset script

--- header.php
+++ header.php
@@ -7,7 +7,10 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 
 		<!-- FONTS -->
-		<script src="https://use.typekit.net/xqo1dlx.js"></script>
+		<!-- <script src="https://use.typekit.net/xqo1dlx.js"></script>
+		<script>try{Typekit.load({ async: true });}catch(e){}</script> -->
+
+		<script src="https://use.typekit.net/edu0ppo.js"></script>
 		<script>try{Typekit.load({ async: true });}catch(e){}</script>
 
 

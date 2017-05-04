b8aa8953b366c531547a4a7e828f0a2f8bb6934f
Omar Rasool <omar@leveragenam.com>
2017-05-04 10:33:40 -0500

    FIXES - minor fixes and tweaks everywhere

--- scripts/smallHelp.js
+++ scripts/smallHelp.js
@@ -1,7 +1,8 @@
 function smallHelp() {
-	
+
 	// Add span around search input field on homepage to add magnifying glass icon
 	$('#searchform').wrapInner("<span class='fieldspan'></span>");
+
 
 }
 
